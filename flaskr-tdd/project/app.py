0f38eeab0deeeb1a0af7fdc64215b7c7684a27c8
Richelle Pereira <richelle0001@gmail.com>
2025-09-28 18:42:19 -0400

    Added some port changes.

--- flaskr-tdd/project/app.py
+++ flaskr-tdd/project/app.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 
 from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort, jsonify
 
@@ -104,6 +105,7 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)
 
 
